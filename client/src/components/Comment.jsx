7c7403313d07c1a6059bcdb2420351fef5b0fea5
abhishektripathi21 <abhishektripathi2111@gmail.com>
2024-06-23 21:42:41 +0530

    add delete functionality to the comment section

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -4,7 +4,7 @@ import { FaThumbsUp } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { Button, Textarea } from 'flowbite-react';
 
-export default function Comment({comment, onLike, onEdit}) {
+export default function Comment({comment, onLike, onEdit, onDelete}) {
     const [user,setUser] = useState({});
     const { currentUser } = useSelector((state) => state.user);
     const [isEditing,setIsEditing] = useState(false);
@@ -48,6 +48,7 @@ export default function Comment({comment, onLike, onEdit}) {
         console.log(error.message);
       }
     };
+
   return (
     <div className='flex p-4 border-b dark:border-gray-600 text-sm'>
       <div className="flex-shrink-0 mr-3">
@@ -85,9 +86,14 @@ export default function Comment({comment, onLike, onEdit}) {
               </p>
               {
                 currentUser && (currentUser._id === comment.userId || currentUser.isAdmin) && (
-                  <button type='button' className='text-gray-400 gover:text-blue-500' onClick={handleEdit}>
-                    Edit
-                  </button>
+                  <>
+                    <button type='button' className='text-gray-400 hover:text-blue-500' onClick={handleEdit}>
+                      Edit
+                    </button>
+                    <button type='button' className='text-gray-400 hover:text-red-500' onClick={() => onDelete(comment._id)}>
+                      Delete
+                    </button>
+                </>
                 )
               }
             </div>
